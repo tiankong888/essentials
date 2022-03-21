87dd8ed13744274f52df05af69626bce45757ee9
tiankong888 <925826693@qq.com>
2022-03-21 21:27:14 +0800

    清理

---
+++ src/app/app.vue
@@ -0,0 +1,18 @@
+<template>
+    <h3>{{name}}</h3>
+</template>
+
+
+<script>
+export default{
+    data(){
+        return {
+            name:'tiankong',
+        }
+    }
+}
+</script>
+
+<style scoped>
+   @import url(./styles/app.css);
+</style>
